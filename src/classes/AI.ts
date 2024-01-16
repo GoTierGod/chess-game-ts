cac519ceb76b8aefc22d15e6b8290c37b4a5406a
GoTierGod <ivan.gunlove_10_@hotmail.com>
2024-01-16 03:27:09 -0300

    Bug fixed in #defPredict method when checking if the enemy King is exposed

--- src/classes/AI.ts
+++ src/classes/AI.ts
@@ -145,7 +145,7 @@ export class PlayerAI {
                                 col: getMoveCol(move),
                                 idx: getMoveIdx(move),
                             },
-                            false
+                            true
                         )
                 )
             }
